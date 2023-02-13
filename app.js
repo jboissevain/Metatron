104553eefb9eca18aa968bfb781641189de9e553
jboissevain <jrdmb1@gmail.com>
2023-02-13 01:41:22 -0500

    cron job and activity decrement function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
-const { Sequelize, DataTypes, Model } = require('sequelize');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const cron = require('node-cron');
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -20,7 +20,7 @@ for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
 
-    if ('data' in command && 'execute' in command){
+    if ('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command);
     } else {
         console.log(`command at ${filePath} is missing a required data or execute property`);
@@ -31,13 +31,28 @@ const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args, client));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args, client));
-	}
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
 }
+
+//Schedule daily activity loss
+cron.schedule('* * * * *', () => {
+    console.log('cron job firing');
+    const guildIDs = client.guilds.cache.map(guild => guild.id);
+    guildIDs.forEach(async guildID => {
+        const guild = client.guilds.resolve(guildID);
+        const members = await guild.members.fetch();
+        members.forEach(async member => {
+            if(!member.user.bot) {
+                await client.db.checkDecrement(member.user.id);
+            }
+        })
+    })
+})
 
 client.login(process.env.TOKEN);
--- database/database.js
+++ database/database.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize('metatron', 'postgres', process.env.PG_PASSWORD,
 });
 
 const MILLISECONDS_PER_MINUTE = 60000;
+const MILLISECONDS_PER_DAY = MILLISECONDS_PER_MINUTE * 86400;
 
 const User = sequelize.define('user', {
     display_name: {
@@ -89,11 +90,35 @@ const getUser = async (userID) => {
             user_id: userID
         }
     })
-    return user;
+    return user[0];
 }
 
 function calculateActivityIncrement(braincells) {
     return 1 + braincells;
+}
+
+const checkDecrement = async (userID) => {
+    const user = await User.findAll({
+        where: {
+            user_id: userID
+        }
+    })
+
+    const activity = user[0].dataValues.activity_score;
+    const newScore = activity - activity*0.05;
+    const lastPost = user[0].dataValues.last_message;
+    const timeSinceLastPost = Math.abs(Date.now() - lastPost);
+
+    if(timeSinceLastPost > MILLISECONDS_PER_DAY) {
+        await User.update({ activity_score: newScore }, {
+            where: {
+                user_id: userID
+            }
+        })
+    } else {
+        await incrementActivity(userID);
+    }
+
 }
 
 module.exports = {
@@ -101,5 +126,6 @@ module.exports = {
     syncModels,
     incrementActivity,
     incrementBraincells,
-    getUser
+    getUser,
+    checkDecrement
 }
